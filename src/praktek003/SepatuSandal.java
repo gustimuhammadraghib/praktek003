071dc319bb70f61abc74f82951d0eab524b0660c
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-04-05 00:14:26 +0800

    Getter and Setter

--- src/praktek003/SepatuSandal.java
+++ src/praktek003/SepatuSandal.java
@@ -6,4 +6,13 @@ public class SepatuSandal extends Sepatu{
     void throttle(){
         System.out.println("------Terima Kasih------");
     }
+
+    public String getBahan() {
+        return bahan;
+    }
+
+    public void setBahan(String bahan) {
+        this.bahan = bahan;
+    }
+    
 }
