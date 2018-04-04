be3950c7b5e326a5205a2186578f9bf383d2f67e
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-04-04 23:53:38 +0800

    Buat Getter and Setter

--- src/praktek003/Sepatu.java
+++ src/praktek003/Sepatu.java
@@ -15,5 +15,45 @@ public class Sepatu {
                 "Kualitas      \t: " +kualitas+"\n"+
                 "Harga         \t: " +harga);
   }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getDesain() {
+        return desain;
+    }
+
+    public void setDesain(String desain) {
+        this.desain = desain;
+    }
+
+    public String getKualitas() {
+        return kualitas;
+    }
+
+    public void setKualitas(String kualitas) {
+        this.kualitas = kualitas;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
   
 }
