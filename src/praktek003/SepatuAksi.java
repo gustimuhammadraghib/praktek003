9a935e587f882584b47d640413c96085ed77fa0f
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-04-05 00:05:24 +0800

    Menggunakan Method Get

--- src/praktek003/SepatuAksi.java
+++ src/praktek003/SepatuAksi.java
@@ -9,5 +9,17 @@ public class SepatuAksi {
         o.setHarga    (2000000);
     
         o.cetakInfo();
+        System.out.println("");
+        System.out.println("=======================");
+        System.out.println("Merknya        \t: ");
+        System.out.println(o.getMerk());
+        System.out.println("Warnanya       \t: ");
+        System.out.println(o.getWarna());
+        System.out.println("Desainnya      \t: ");
+        System.out.println(o.getDesain());
+        System.out.println("Kualitasnya    \t: ");
+        System.out.println(o.getKualitas());
+        System.out.println("Harganya       \t: ");
+        System.out.println(o.getHarga());
     }
 }
