f9b5a88c8bd3132c97e60c87042a1eeba3b59f9f
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-04-05 00:12:44 +0800

    Attribute dan Method

--- src/praktek003/SepatuSandal.java
+++ src/praktek003/SepatuSandal.java
@@ -1,5 +1,9 @@
 package praktek003;
 
 public class SepatuSandal extends Sepatu{
+    private String bahan;
     
+    void throttle(){
+        System.out.println("------Terima Kasih------");
+    }
 }
