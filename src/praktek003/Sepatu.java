41e3fb04bcf144b1b64d5ec1456c29b6b602afe8
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-04-04 23:35:07 +0800

    Buat Method CetakInfo()

--- src/praktek003/Sepatu.java
+++ src/praktek003/Sepatu.java
@@ -2,8 +2,17 @@ package praktek003;
 
 public class Sepatu {
   String merk;
+  String warna;
   String desain;
   String kualitas;
   double harga;
+  
+  void cetakInfo(){
+      System.out.println("Merk \t: "+merk+"\n"+
+                "Warna         \t: "+warna+"\n"+
+                "Desain        \t: "+desain+"\n"+
+                "Kualitas      \t: "+kualitas+"\n"+
+                "Harga         \t: "+harga);
+  }
   
 }
